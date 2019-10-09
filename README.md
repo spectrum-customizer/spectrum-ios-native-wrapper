815ac9d9f37840e19cea789e48119827e3c6eaa5
TomFalkner <tafalkner@gmail.com>
2019-10-09 16:33:26 -0700

    Update readme, cleanup code

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-iOS Native Wrapper for Spectrum
-===============================
+Spectrum Wrapper for iOS
+========================
+
+Overview
+--------
+
+The Spectrum Customizer library provides a wrapper around a UIView with helper methods for interacting with Spectrum Customizer Content.
+
+Adding the library to a project
+-------------------------------
+
+First, download the project from TK. Then, in XCode:
+
+1. Click on the project
+2. Click on embedded binaries
+3. Navigate to the Spectrum Customizer project
+4. Click Add XCode project
+
+Adding a SpectrumView in Interface Builder
+------------------------------------------
+
+Drag an instance of UIView from the Library onto the Storyboard. Select the new UIView and open the Identity Inspector. Change the class to 'SpectrumCustomizerView' and the module to Spectrum Customizer. Add an IBOutlet to the UIViewController that will be responsible for the SpectrumCustomizerView.
+
+Initializing the Customizer
+---------------------------
+
+Use the SpectrumCustomizerView's loadCustomizer method to load the application. Internally the SpectrumCustomizerView has an HTML page that implements the necessary methods that Spectrum needs to interact with the host environment.
+
+```swift
+
+spectrum.loadCustomizer(customizerUrl: "https://madetoorderdev.blob.core.windows.net/spectrum-native-test/app.js")
+
+```
+
+Set up the SpectrumViewDelegate
+-------------------------------
+
+Implementing SpectrumViewDelegate is necessary in order to add an item to the cart and to provide the customizer with pricing data.
+
+```swift
+
+public protocol SpectrumCustomizerViewDelegate: class {
+  func addToCart(sender: SpectrumCustomizerView, skus: [String], recipeSetId: String, options: [String: String])
+  func getPrice(sender: SpectrumCustomizerView, skus: [String], options: [String: String]) -> [String: SpectrumPrice]
+}
+
+```
+
+Here is the implementation of SpectrumPrice:
+
+```
+
+public struct SpectrumPrice: Codable {
+  public var price: String
+  public var inStock: Bool
+
+  public init(price: String, inStock: Bool) {
+    self.price = price
+    self.inStock = inStock
+  }
+}
+
+```
+
+Loading a recipe or a product
+-----------------------------
+
+Existing recipes can be loaded by calling loadRecipe and products can be loaded by calling loadSku. Both expect a SpectrumArguments object:
+
+```swift
+
+// recipe
+let arg = SpectrumArguments(fromRecipeId: "P2LD123")
+spectrum.loadRecipe(args: arg)
+
+// Product
+let arg = SpectrumArguments(fromSku: "example-pro-product-1")
+spectrum.loadSku(args: arg)
+
+```
+
+
+Example Implementation
+----------------------
+
+A simple example implementation (SpectrumIntegration) can be found in the repo.
--- SpectrumCustomizer/SpectrumCustomizer/SpectrumCustomizerView.swift
+++ SpectrumCustomizer/SpectrumCustomizer/SpectrumCustomizerView.swift
@@ -21,50 +21,48 @@ public class SpectrumCustomizerView: UIView, WKNavigationDelegate, WKScriptMessa
 
   @IBOutlet var contentView: UIView!
   @IBOutlet weak var webView: WKWebView!
-  
+
   weak public var delegate:SpectrumCustomizerViewDelegate?
-  
+
   let nibName = "SpectrumCustomizerView"
- 
+
   var customizerSource = ""
   var webViewReady = false
   var webSourceLoaded = false
-  
+
   public override init(frame: CGRect) {
     super.init(frame: frame)
     setUpView()
   }
-  
+
   public required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     setUpView()
   }
-  
+
   public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
     if !webSourceLoaded && customizerSource != "" {
       load(customizerUrl: customizerSource)
       webSourceLoaded = true
     }
   }
-  
+
   private func setUpView() {
     guard let bundle = Bundle(identifier: "com.pollinate.SpectrumCustomizer") else { return }
-    
+
     bundle.loadNibNamed("SpectrumCustomizerView", owner: self, options: nil)
-    
+
     addSubview(contentView)
     contentView.frame = self.bounds
     self.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-    
+
     webView.navigationDelegate = self;
     webView.configuration.userContentController.add(self, name: addToCart)
      webView.configuration.userContentController.add(self, name: getPrice)
     guard let url = bundle.url(forResource: "index", withExtension: "html", subdirectory: nil) else { return }
     webView.loadFileURL(url, allowingReadAccessTo: url)
   }
- 
-  
-  
+
   public func loadCustomizer(customizerUrl: String) {
     if webViewReady {
       load(customizerUrl: customizerUrl)
@@ -73,26 +71,26 @@ public class SpectrumCustomizerView: UIView, WKNavigationDelegate, WKScriptMessa
       customizerSource = customizerUrl
     }
   }
-  
+
   public func loadRecipe(args: SpectrumArguments) {
-    
+
     let jsonEncoder = JSONEncoder();
-    
+
     do {
       let jsonData = try jsonEncoder.encode(args)
       let jString = String(data: jsonData, encoding: .utf8)
-      
-      let serialized = "integration.loadRecipe('\(jString!)')"
-      
+
+      let serialized = "spectrum.loadRecipe('\(jString!)')"
+
       webView.evaluateJavaScript(serialized, completionHandler: {(html: AnyObject?, error: NSError?) in
         print(html!)
         } as? (Any?, Error?) -> Void)
-      
+
     } catch {
       print("error loading recipe")
     }
   }
-  
+
   func load(customizerUrl: String) {
     let script = "loadCustomizer('" + customizerUrl + "')"
        webView.evaluateJavaScript(script, completionHandler: {(html: AnyObject?, error: NSError?) in
@@ -102,24 +100,24 @@ public class SpectrumCustomizerView: UIView, WKNavigationDelegate, WKScriptMessa
   /**
    */
   public func loadSku(args: SpectrumArguments) {
-    
+
     let jsonEncoder = JSONEncoder();
-    
+
     do {
       let jsonData = try jsonEncoder.encode(args)
       let jString = String(data: jsonData, encoding: .utf8)
-      
-      let serialized = "integration.loadSku('\(jString!)')"
-      
+
+      let serialized = "spectrum.loadSku('\(jString!)')"
+
       webView.evaluateJavaScript(serialized, completionHandler: {(html: AnyObject?, error: NSError?) in
         print(html!)
         } as? (Any?, Error?) -> Void)
-      
+
     } catch {
       print("error loading recipe")
     }
   }
-  
+
   public func userContentController(_ userContentController: WKUserContentController,
                              didReceive message: WKScriptMessage) {
     if message.name == addToCart {
@@ -129,25 +127,26 @@ public class SpectrumCustomizerView: UIView, WKNavigationDelegate, WKScriptMessa
         let options = dict["options"] as? [String: String] else {
           return
       }
-      
+
       delegate?.addToCart(sender: self, skus: skus, recipeSetId: recipeSetId, options: options)
-      
+
     } else if message.name == getPrice {
-    
+
       guard let dict = message.body as? [String: AnyObject],
         let callbackId = dict["callbackId"] as? Int,
         let skus = dict["skus"] as? [String],
         let options = dict["options"] as? [String: String] else {
           return
       }
-      
+
       let prices = delegate?.getPrice(sender: self, skus: skus, options: options)
-      
+
+
       let jsonEncoder = JSONEncoder()
       if let unWrappedPrices = prices,
         let pricesJson = try? jsonEncoder.encode(unWrappedPrices),
         let pricesText = String(data: pricesJson, encoding: String.Encoding.utf8) {
-      
+
         let script = "resolvePrice(" + String(callbackId) + ", " + pricesText + ");"
         webView.evaluateJavaScript(script, completionHandler: {(html: AnyObject?, error: NSError?) in
         print(html!)
